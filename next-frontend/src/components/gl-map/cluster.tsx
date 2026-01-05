4f3e56710df00c7c8ccfbd374cf2a2cb14d3d785
Saksorn Malasuttichai <s.malasutt@gmail.com>
2026-01-06 03:30:26 +0700

    update map cluster and restriction

--- next-frontend/src/components/gl-map/cluster.tsx
+++ next-frontend/src/components/gl-map/cluster.tsx
@@ -1,83 +1,126 @@
 import { Pin, AdvancedMarker, useMap } from "@vis.gl/react-google-maps";
 import { MarkerClusterer } from "@googlemaps/markerclusterer";
 import type { Marker } from "@googlemaps/markerclusterer";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Polygon } from "./geometry/polygon";
 
 type Poi = { key: string; location: google.maps.LatLngLiteral };
 const locations: Poi[] = [
-	{ key: "pos-1", location: { lat: 13.846473, lng: 100.5667563 } },
-	{ key: "pos-2", location: { lat: 13.8472767, lng: 100.568164 } },
-	{ key: "pos-3", location: { lat: 13.8209738, lng: 100.563253 } },
-	{ key: "pos-4", location: { lat: 13.8690081, lng: 100.562393 } },
-	{ key: "pos-5", location: { lat: 13.8587568, lng: 100.5658246 } },
-	{ key: "pos-6", location: { lat: 13.858761, lng: 100.565688 } },
-	{ key: "pos-7", location: { lat: 13.852228, lng: 100.5708374 } },
-	{ key: "pos-8", location: { lat: 13.8537375, lng: 100.5722569 } },
-	{ key: "pos-9", location: { lat: 13.854167, lng: 100.5716387 } },
-	{ key: "pos-10", location: { lat: 13.8536005, lng: 100.56092542 } },
-	{ key: "pos-11", location: { lat: 13.859395, lng: 100.56198648 } },
-	{ key: "pos-12", location: { lat: 13.8565445, lng: 100.5980808 } },
-	{ key: "pos-13", location: { lat: 13.859627, lng: 100.582146 } },
-	{ key: "pos-14", location: { lat: 13.85488, lng: 100.5887113 } },
-	{ key: "pos-15", location: { lat: 13.8505523, lng: 100.5772205 } },
+	{ key: "poi-1", location: { lat: 13.8515, lng: 100.572 } },
+	{ key: "poi-2", location: { lat: 13.852, lng: 100.575 } },
+	{ key: "poi-3", location: { lat: 13.853, lng: 100.578 } },
+	{ key: "poi-4", location: { lat: 13.854, lng: 100.579 } },
+	{ key: "poi-5", location: { lat: 13.855, lng: 100.574 } },
+	{ key: "poi-6", location: { lat: 13.856, lng: 100.571 } },
+	{ key: "poi-7", location: { lat: 13.857, lng: 100.577 } },
+	{ key: "poi-8", location: { lat: 13.858, lng: 100.573 } },
+	{ key: "poi-9", location: { lat: 13.859, lng: 100.576 } },
+	{ key: "poi-10", location: { lat: 13.8545, lng: 100.5725 } },
+	{ key: "poi-11", location: { lat: 13.8518, lng: 100.578 } },
+	{ key: "poi-12", location: { lat: 13.8525, lng: 100.571 } },
+	{ key: "poi-13", location: { lat: 13.8535, lng: 100.575 } },
+	{ key: "poi-14", location: { lat: 13.8565, lng: 100.579 } },
+	{ key: "poi-15", location: { lat: 13.8575, lng: 100.573 } },
+	{ key: "poi-16", location: { lat: 13.8585, lng: 100.577 } },
+	{ key: "poi-17", location: { lat: 13.8555, lng: 100.574 } },
+	{ key: "poi-18", location: { lat: 13.8595, lng: 100.571 } },
+	{ key: "poi-19", location: { lat: 13.8542, lng: 100.576 } },
+	{ key: "poi-20", location: { lat: 13.8532, lng: 100.572 } },
 ];
 
 export const Cluster = () => {
 	const map = useMap();
 	const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
-	const clusterer = useRef<MarkerClusterer | null>(null);
-
-	const handleClick = (e: google.maps.MapMouseEvent) => {
-		console.log("Map clicked at: ", e.latLng?.toString());
-	};
-
-	useEffect(() => {
-		if (!map) return;
-		if (!clusterer.current) {
-			clusterer.current = new MarkerClusterer({ map });
-		}
+	// const clusterer = useRef<MarkerClusterer | null>(null);
+	const clusterer = useMemo(() => {
+		if (!map) return null;
+		return new MarkerClusterer({ map });
 	}, [map]);
 
+	// const handleClick = (e: google.maps.MapMouseEvent) => {
+	// 	console.log("Map clicked at: ", e.latLng?.toString());
+	// };
+
+	// useEffect(() => {
+	// 	if (!map) return;
+	// 	if (!clusterer.current) {
+	// 		clusterer.current = new MarkerClusterer({ map });
+	// 	}
+	// }, [map]);
+
 	useEffect(() => {
-		if (!map || !clusterer.current) return;
-		clusterer.current.clearMarkers();
-		clusterer.current.addMarkers(Object.values(markers));
-	}, [markers, map]);
+		if (!clusterer) return;
+		clusterer.clearMarkers();
+		clusterer.addMarkers(Object.values(markers));
+	}, [clusterer, markers]);
 
-	const setMarkerRef = (marker: Marker | null, key: string) => {
-		if (marker && markers[key]) return;
-		if (!marker && !markers[key]) return;
+	const setMarkerRef = useCallback((marker: Marker | null, key: string) => {
+		// if (marker && markers.current[key]) return;
+		// if (!marker && !markers.current[key]) return;
 
+		// setMarkers((prev) => {
+		// 	if (marker) {
+		// 		return { ...prev, [key]: marker };
+		// 	} else {
+		// 		const newMarkers = { ...prev };
+		// 		delete newMarkers[key];
+		// 		return newMarkers;
+		// 	}
+		// });
 		setMarkers((prev) => {
+			if ((marker && prev[key]) || (!marker && !prev[key])) return prev;
+
 			if (marker) {
 				return { ...prev, [key]: marker };
 			} else {
-				const newMarkers = { ...prev };
-				delete newMarkers[key];
+				const { [key]: _, ...newMarkers } = prev;
 				return newMarkers;
 			}
 		});
-	};
+	}, []);
 
 	return (
 		<>
 			{locations.map((poi) => (
-				<AdvancedMarker
+				// <AdvancedMarker
+				// 	key={poi.key}
+				// 	position={poi.location}
+				// 	ref={(marker) => setMarkerRef(marker, poi.key)}
+				// 	clickable
+				// 	onClick={handleClick}
+				// >
+				// 	<Pin
+				// 		background={"#FBBC04"}
+				// 		glyphColor={"#000"}
+				// 		borderColor={"#000"}
+				// 	/>
+				// </AdvancedMarker>
+				<CustomMarker
 					key={poi.key}
+					name={poi.key}
 					position={poi.location}
-					ref={(marker) => setMarkerRef(marker, poi.key)}
-					clickable
-					onClick={handleClick}
-				>
-					<Pin
-						background={"#FBBC04"}
-						glyphColor={"#000"}
-						borderColor={"#000"}
-					/>
-				</AdvancedMarker>
+					setMarkerRef={setMarkerRef}
+				/>
 			))}
 		</>
+	);
+};
+
+const CustomMarker = (props: {
+	name: string;
+	position: google.maps.LatLngLiteral;
+	setMarkerRef: (marker: Marker | null, key: string) => void;
+}) => {
+	const { name, position, setMarkerRef } = props;
+	const ref = useCallback(
+		(marker: google.maps.marker.AdvancedMarkerElement) =>
+			setMarkerRef(marker, name),
+		[setMarkerRef, name]
+	);
+	return (
+		<AdvancedMarker position={position} ref={ref} clickable>
+			<Pin />
+		</AdvancedMarker>
 	);
 };
 
--- next-frontend/src/components/gl-map/google-map.tsx
+++ next-frontend/src/components/gl-map/google-map.tsx
@@ -4,7 +4,7 @@ import { APIProvider, Map, ColorScheme } from "@vis.gl/react-google-maps";
 import { useTheme } from "next-themes";
 import { Cluster, TestPolygon } from "./cluster";
 
-export function GoogleMap() {
+export default function GoogleMap() {
 	const API_KEY =
 		process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? GOOGLE_MAPS_API_KEY;
 
@@ -17,16 +17,26 @@ export function GoogleMap() {
 		: ColorScheme.FOLLOW_SYSTEM;
 
 	return (
-		<div className="size-full">
+		<div className="w-dvw max-w-full max-h-full aspect-square">
 			<APIProvider apiKey={API_KEY} onLoad={() => console.log("Load map...")}>
 				<Map
 					reuseMaps={true}
 					mapId={"358498466570083911ca77a2"}
 					colorScheme={colorScheme}
 					defaultZoom={15}
+					minZoom={14}
 					defaultCenter={{ lat: 13.85003, lng: 100.57099 }}
 					gestureHandling={"greedy"}
-					// disableDefaultUI={true}
+					disableDefaultUI={true}
+					restriction={{
+						latLngBounds: {
+							north: 13.875,
+							south: 13.83,
+							east: 100.6,
+							west: 100.55,
+						},
+						strictBounds: true,
+					}}
 					onClick={(e) => console.log(e)}
 				>
 					<Cluster />
