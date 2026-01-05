eb74942f2d56ef916ee139c4c1ef99f7a4b66da4
Saksorn Malasuttichai <s.malasutt@gmail.com>
2026-01-06 03:34:03 +0700

    update test

---
+++ next-frontend/e2e/building.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("test building page", () => {
+	test("Building Image Carousel displays control button", async ({ page }) => {
+		await page.goto("/dashboard/1");
+		const ImageCarousel = page.getByTestId("building-image-carousel").last();
+		await expect(ImageCarousel).toBeVisible();
+		await ImageCarousel.focus();
+		const previous = ImageCarousel.getByTestId(
+			"building-image-carousel-previous"
+		);
+		const next = ImageCarousel.getByTestId("building-image-carousel-next");
+		if ((await previous.isVisible()) && (await next.isVisible())) {
+			await expect(previous).toBeEnabled();
+			await expect(next).toBeEnabled();
+		} else {
+			await ImageCarousel.hover();
+			await expect(previous).toBeVisible();
+			await expect(next).toBeVisible();
+			await expect(previous).toBeEnabled();
+			await expect(next).toBeEnabled();
+		}
+	});
+});
--- next-frontend/e2e/nav.spec.ts
+++ next-frontend/e2e/nav.spec.ts
@@ -3,35 +3,26 @@ import { test, expect } from "@playwright/test";
 test.describe("test navigation", () => {
 	test("Home page navigation Link", async ({ page }) => {
 		await page.goto("/");
-		const link = page.getByRole("link", { name: "Go to About Page" });
+		const link = page.getByRole("link", { name: "Dashboard" });
 		await expect(link).toBeVisible();
 		await link.click();
-		await expect(page).toHaveURL("/about");
-		await expect(page.getByRole("heading", { name: "About Us" })).toBeVisible();
-		await expect(page.getByText("Welcome to the About Us page.")).toBeVisible();
-		await expect(
-			page.getByRole("link", { name: "Go back to Home" })
-		).toBeVisible();
-		await expect(
-			page.getByRole("link", { name: "Go back to Home" })
-		).toBeEnabled();
-		const button = page.getByRole("button", { name: "Disabled" });
-		await expect(button).toBeVisible();
-		await expect(button).toBeDisabled();
+		await expect(page).toHaveURL("/dashboard");
 	});
 
 	test("About page navigation Link", async ({ page }) => {
 		await page.goto("/about");
+		await expect(page.getByRole("heading", { name: "About Us" })).toBeVisible();
+		await expect(page.getByText("Welcome to the About Us page.")).toBeVisible();
 		const link = page.getByRole("link", { name: "Go back to Home" });
 		await expect(link).toBeVisible();
+		await expect(link).toBeEnabled();
+		const button = page.getByRole("button", { name: "Disabled" });
+		await expect(button).toBeVisible();
+		await expect(button).toBeDisabled();
 		await link.click();
 		await expect(page).toHaveURL("/");
-		await expect(
-			page.getByRole("link", { name: "Go to About Page" })
-		).toBeVisible();
-		await expect(
-			page.getByRole("link", { name: "Go to About Page" })
-		).toBeEnabled();
+		await expect(page.getByRole("link", { name: "Dashboard" })).toBeVisible();
+		await expect(page.getByRole("link", { name: "Dashboard" })).toBeEnabled();
 		await page.goBack();
 		await expect(page).toHaveURL("/about");
 	});
