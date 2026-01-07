0b2c507d9ca654198efb6c0fbb9001d617311029
Saksorn Malasuttichai <s.malasutt@gmail.com>
2026-01-08 01:44:34 +0700

    modal template

---
+++ next-frontend/src/components/modal/modal.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export const Modal = ({
+	children,
+}: // onClose
+{
+	children: React.ReactNode;
+	// onClose: () => void;
+}) => {
+	// if (!isOpen) return null;
+	const router = useRouter();
+	const handleClickOutside = (e: React.MouseEvent) => {
+		if (e.target === e.currentTarget) {
+			router.back();
+		}
+	};
+
+	return (
+		<div
+			className="fixed inset-0 z-50 flex items-center justify-center bg-black/80"
+			onClick={handleClickOutside}
+		>
+			<div className="relative bg-popover rounded-2xl p-6 w-[75vw] max-w-md shadow-xl text-popover-foreground overflow-auto">
+				{/* Close button */}
+				<button
+					onClick={() => router.back()}
+					className="absolute text-2xl top-2 right-4 text-gray-500 hover:text-gray-700"
+				>
+					×
+				</button>
+				{children}
+			</div>
+		</div>
+	);
+};
