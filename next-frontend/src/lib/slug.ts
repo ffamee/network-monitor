94ac6507256a1093cd6ac30565074928c192cf72
Saksorn Malasuttichai <s.malasutt@gmail.com>
2026-01-25 21:39:21 +0700

    checking invalid slug format

--- next-frontend/src/lib/slug.ts
+++ next-frontend/src/lib/slug.ts
@@ -1,4 +1,8 @@
 export function getIdFromSlug(slug: string): string | null {
 	const parts = slug.split("-");
+	if (parts.length < 2) {
+		// Invalid slug format
+		return null;
+	}
 	return parts[0] || null;
 }
